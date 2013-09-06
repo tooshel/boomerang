595e6d9c15e3c28dd0010024ad9d8be6f869bfcc
Sheldon McGee <sheldon@tooshel.com>
2013-09-06 12:46:52 -0700

    this is the real commit

--- js/config.js
+++ js/config.js
@@ -1,16 +1,16 @@
 boomerang.factory('Config',function(){
     return {
         //modify these
-        'name'          : 'GDG Fresno',
-        'id'            : '114769570436363155784',
+        'name'          : 'Phoenix GDG',
+        'id'            : '107371179007921028529',
         'google_api'    : 'AIzaSyDPjRfTjr-X-FB0jRSf06_7rHAvu9oQ3To',
         'pwa_id'        : '5846413253595166705', //picasa web album id, must belong to google+ id above
         'cover' : {
-            title : 'DevFest Fresno',
-            subtitle : 'The largest Google Developer Conference in the Central Valley',
+            title : 'GDG DevFest Southwest',
+            subtitle : 'Google Developer Conference focused on DevTools',
             button : {
-                text : 'Register',
-                url : 'https://devfestfresno.eventbrite.com'
+                text : 'Join Us',
+                url : 'https://plus.google.com/u/0/events/c68v2er7vfap2288bthdr5a0vv4'
             }
         }
     }
